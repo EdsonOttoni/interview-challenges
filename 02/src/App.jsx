9151daef5eab1614032c10c558eb5934d495f84e
EdsonOttoni <edson.ottoni@gmail.com>
2023-11-28 21:48:58 -0300

    finishing challenge 02

--- 02/src/App.jsx
+++ 02/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css'
 
 function App() {
   const [position, setPosition] = useState([])
-  
+  const [unDid, setUnDid] = useState([])
+
   function handleClick(event) {
     const newDot = {
       positionX: event.pageX,
@@ -11,36 +12,55 @@ function App() {
     }
 
     setPosition((prev) => [...prev, newDot])
+    setUnDid([])
   }
 
   function handleUndo() {
     if (position.length === 0) {
+      return
+    }
+
+    const lastPosition = position[position.length - 1];
+    setUnDid((prev) => [...prev, lastPosition]);
+
+    setPosition((prev) => {
+      const newArr = [...prev].slice(0, -1)
+      return newArr
+    })
+  }
+
+  function handleRedo() {
+    if (unDid.length === 0) {
       return;
     }
 
-    setPosition((prev) => {
+    const recoveredDot = unDid[unDid.length - 1];
+    setUnDid((prev) => {
       const newArr = [...prev].slice(0, -1);
       return newArr;
     });
-  }
-
-  function handleRedo(event) {
-    console.log('redo')
+    setPosition((prev) => [...prev, recoveredDot]);
   }
 
   return (
-    <div id="page" onClick={handleClick}>
-      <button type="button" onClick={handleUndo}>Desfazer</button>
-      <button type="button" onClick={handleRedo}>Refazer</button>
+    <>
+      <button type="button" onClick={handleUndo}>
+        Desfazer
+      </button>
+      <button type="button" onClick={handleRedo}>
+        Refazer
+      </button>
 
-      {position.map((item, index) => (
-        <span
-          key={index}
-          className='dot'
-          style={{ left: item.positionX, top: item.positionY }}
-        />
-      ))}
-    </div>
+      <div id="page" onClick={handleClick}>
+        {position.map((item, index) => (
+          <span
+            key={index}
+            className="dot"
+            style={{ left: item.positionX, top: item.positionY }}
+          />
+        ))}
+      </div>
+    </>
   )
 }
 
